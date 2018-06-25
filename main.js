dfdfb015071e4181921accb4386f0ade98d08c28
DrSmile444 <drsmile444@gmail.com>
2018-06-25 13:54:09 +0300

    many useful changes

--- main.js
+++ main.js
@@ -1,106 +1,233 @@
-'use strict'; let p = console.log;
+"use strict";
 
-let output = document.getElementById('output');
+var p = console.log;
+var output = document.getElementById("output");
 
 function start(key, flag) {
-  if (key) {
-    output.value = toDecimal( prompt('Pick number: (binary)',''), flag )
-  } else {
-    output.value = toBinary( +prompt('Pick number:',''), flag );
+  outputDefault();
+  if (key == "decimal") {
+    //*** */
+    //pick number
+    //*** */
+    var number = prompt("Pick number: (binary)", "");
+    var checkedFloat = parseNumber(number).length == 2;
+
+    if (!isNumber(number) || checkedFloat) {
+      output.value = "This is not a number or zero!";
+      return 1;
+    }
+
+    output.value = toDecimal(number, flag);
+  } else if (key == "binary") {
+    //*** */
+    //pick number
+    //*** */
+    var _number = prompt("Pick number:", "");
+    if (!isNumber(_number)) {
+      output.value = "This is not a number or zero!";
+      return 1;
+    }
+
+    _number = parseNumber(_number);
+    var _checkedFloat = _number.length == 2;
+
+    if (_checkedFloat && flag && _number[0] == 0) {
+      output.value = "This is not a number or zero!";
+      return 1;
+    }
+
+    var binReal = void 0;
+    var binFloat = void 0;
+
+    if (_number[0] == 0) {
+      binReal = ["", 0];
+    } else {
+      binReal = toBinary(+_number[0], flag, _checkedFloat);
+    }
+
+    output.value = binReal[0];
+    if (_number[1] && !flag) {
+      var accuracy = prompt("Pick precision in numbers:\n0.(count)", "");
+      if (!isNumber(accuracy)) {
+        accuracy = 4;
+      }
+
+      binFloat = toBinaryFloat(_number[1], accuracy);
+      output.value += binFloat[0];
+      output.value += "\n\nFinal:  \n" + binReal[1] + "." + binFloat[1];
+    }
+  }
+  if (output.value.length > 60) {
+    output.style.height = "36vh";
   }
 }
 
-function toBinary(num, flag) {
-  outputDefault();
-  let outputBin = '';
-
-  if (isNaN(+num) ){
-    outputBin = 'This is not a number!';
-    return 1;
+function parseNumber(num) {
+  num = num + "";
+  var result1 = num.split(".");
+  var result2 = num.split(",");
+  if (result1.length == 2) {
+    return result1;
   }
-  
-  let result = [];
+  return result2;
+}
+
+function toBinary(num, flag, float) {
+  //flag is responsible for minus binary
+  var outputBin = "";
+  var result = [];
 
   if (flag) {
-    let remainder = Math.abs(num % 2), 
-        interval = num - remainder,
-        indent = (interval + '').length + 1;
-    outputBin += num + '|' + addSpace(interval, indent) + '|M\n';
+    var remainder = Math.abs(num % 2),
+      interval = num - remainder,
+      indent = (interval + "").length + 1;
+    outputBin += num + "|" + addSymbol(interval, indent, " ") + "|M\n";
 
-    result.push(remainder)
+    result.push(remainder);
 
-    while(0 < Math.abs(num) ) {
-      num = (num - result[result.length-1]) / -2;
+    while (0 < Math.abs(num)) {
+      num = (num - result[result.length - 1]) / -2;
 
-      result.push( Math.abs(num % 2) );
+      result.push(Math.abs(num % 2));
 
-      interval = (num - result[result.length-1]);
+      interval = num - result[result.length - 1];
 
-      outputBin += num + '|'+ addSpace(interval, indent) + '|' + result[result.length-2] +'\n';
+      outputBin +=
+        num +
+        "|" +
+        addSymbol(interval, indent, " ") +
+        "|" +
+        result[result.length - 2] +
+        "\n";
     }
 
-    result = result.slice(0, result.length-1)
+    result = result.slice(0, result.length - 1);
   } else {
-    outputBin += num + '|B\n';
+    outputBin += num + "|B\n";
 
-    while(0 < num) {
-      let remainder = num % 2;
-      result.push(remainder);
-      num = Math.floor(num/2);
-      outputBin += num + '|'+ remainder + '\n';
+    while (0 < num) {
+      var _remainder = num % 2;
+      result.push(_remainder);
+      num = Math.floor(num / 2);
+      outputBin += num + "|" + _remainder + "\n";
     }
-  }  
-  
-  if (output.value.length > 37) {
-    output.style.height = '220px';
   }
-  if (output.value.length > 50) {
-    output.style.height = '270px';
+  result = result.reverse().join("");
+
+  if (!float || flag) {
+    outputBin += "Result:  \n" + result;
+  } else {
+    outputBin += "\n";
+  }
+  return [outputBin, result];
+}
+
+function toBinaryFloat(num, accuracy) {
+  var result = [];
+  var text = ["0." + num + "|B"];
+  var stage = (num + "").length + 1;
+  accuracy = accuracy;
+
+  for (var i = 0; i < accuracy; i++) {
+    num = +num * 2 + "";
+    //fix empty sybmols after multiply
+    num = addSymbol(num, stage - 1, "0");
+    var temp = ["0.", num];
+
+    switch (num.length) {
+      case stage:
+        num = (num + "").slice(1, stage);
+        temp[0] = "1.";
+        temp[1] = num;
+        result.push(1);
+        break;
+
+      case stage - 2:
+        //fix empty symbol
+        temp[1] = num + "0";
+        break;
+
+      default:
+        result.push(0);
+    }
+
+    temp.push("|" + result[i]);
+    text.push(temp.join(""));
   }
 
-  result = result.reverse().join('');
-  outputBin += 'Result:  \n' + result;
-  return outputBin;
+  return [text.join("\n"), result.join("")];
 }
 
 function toDecimal(num, flag) {
-  outputDefault();
-  let process = (num + '').split('').reverse();
-  let result = 0, arr = [], minus = '';
+  var process = (num + "").split("").reverse();
+  var result = 0,
+    arr = [],
+    minus = "";
 
-  for (let i = 0, n = process.length; i < n; i++) {
-    if (+process[i]) {
-      minus = '';
+  for (var i = 0, n = process.length; i < n; i++) {
+    if (+process[i] == 0 || +process[i] == 1) {
+      minus = "";
 
-      if(flag) {
-        if(i % 2 == 1) minus = '-';
+      if (flag) {
+        if (i % 2 == 1) minus = "-";
       }
 
-      let chache = +(minus + Math.pow(2, i) );
+      var chache = +(minus + Math.pow(2, i));
       result += chache;
-      arr.push(chache)
+      arr.push(chache);
+    } else {
+      return "Invalid number!\n Use only 1 and 0.";
     }
   }
 
-  return num + ' =\n' + arr.reverse().join(' + ') + '\nResult is:   \n' + result;
+  return (
+    +num + " =\n" + arr.reverse().join(" + ") + "\nResult is:   \n" + result
+  );
+}
+
+//*** */
+// additional funcs
+//*** */
+
+function isNumber(number) {
+  var temp = "";
+  try {
+    if (!!~number.indexOf(",")) {
+      if (number[0] == ",") {
+        number = "." + number.slice(1, number.length);
+      } else {
+        temp += parseInt(number);
+        temp += "." + number.slice(temp.length + 1, number.length);
+        number = temp;
+      }
+    }
+  } finally {
+    return !(isNaN(+number) || number == 0 || number == null);
+  }
 }
 
 function outputDefault() {
-  output.value = '';
-  output.style.height = '200px';
+  output.value = "";
+  output.style.height = "200px";
 }
 
-function addSpace(string, count) {
-  string += '';
-  let arr = string.split('').reverse();
-  for(let i = 0, n = count - string.length; i < n; i++) {
-    arr.push(' ');
+function addSymbol(string, count, symbol) {
+  string = string + "";
+  var strLeng = string.length;
+  var temp = "";
+
+  if (strLeng < count) {
+    for (var i = 0, n = count - strLeng; i < n; i++) {
+      temp += symbol + "";
+    }
   }
-  return arr.reverse().join('');
+
+  return temp + string;
 }
 
 function about() {
-  let info = '\n   The program helps understand the principle of obtaining a binary number.\n\n   New function: you can get a number in the negative binary numeric system!\n\nAnyway, you can obtain a decimal number.';
-  
+  var info =
+    "\n   The program helps understand the principle of obtaining a binary number.\n\nFeatures: \n - negative system\n - float numbers\n\nAnyway, you can obtain a decimal number.";
+
   alert(info);
 }
---
+++ readme.md
@@ -0,0 +1,15 @@
+# To Binary
+
+### -- *Also avilable [live](https://drsmile444.github.io/toBinary/) at github pages!*
+
+The program designed for convert decimal numbers to binary system.
+
+With this program, you can understand how to translate numbers into a binary system, vice versa, and also get acquainted with the minus binary system number!
+
+## Features:
+- ### negative system;
+- ### float numbers
+
+If you like and you benefit from this program, leave me a star!
+
+[![N|DrSmile](https://drsmile444.github.io/CS50-Final-Project/img/readmeMD.png)](https://github.com/DrSmile444)
